140e163445c99403f5e4ed7df9e4aec5bdff6017
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-02-02 11:42:44 -0500

    commit tree 6d617e7f68a9fa2d3b21d1aa94bd6e91f4e2cc07

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :no_click_bait
+
+    def no_click_bait
+        if title && !title.include?("Won't Believe" || "Secret" ||"Guess" ||"Top [number]")
+          errors.add(:title, "Not clickbait")
+        end
+    end
+
 end
